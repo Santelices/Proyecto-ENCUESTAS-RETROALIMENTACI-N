b169060ac1c29136de8252dc72b557b7207472e2
Cristian <tu_correo@example.com>
2024-10-28 23:37:36 -0500

    Actualizar config

--- backend/config.py
+++ backend/config.py
@@ -3,8 +3,8 @@ import os
 class ConfigBase:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_SORT_KEYS = False
-    SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")  # Añade una clave predeterminada solo para desarrollo local
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "default-jwt-secret-key")
+    SECRET_KEY = os.getenv("SECRET_KEY", '280a818e6b63d88ee0c2d618')  
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", '107c0b17bcff75d5a3653549')
 
 class DesarrolloConfig(ConfigBase):
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///EncuestasBD.db")
